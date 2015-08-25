479646917568eb5d5dd5fbde8fd7df38a84e3fe2
MVB <41804112@SCI-6047.mqauth.uni.mq.edu.au>
2015-08-25 18:17:58 +1000

    v0.2 restructure + added FDI and ratings

--- bomScrape.py
+++ bomScrape.py
@@ -1,8 +1,11 @@
-__author__ = 'AP+MVBedit'
-
+__author__ = 'Alex Parkinson, Matt van Breugel'
+#==============================================================================
 global version
-version = 'v0.1'
+version = 'v0.2'
 
+print('>>> FIRE WEATHER <<<').center(80, ' ')
+
+#==============================================================================
 import urllib2
 from lxml import html
 from datetime import datetime, date, timedelta
@@ -11,12 +14,13 @@ import pyFWI.FWIFunctions as FWI
 import sqlite3
 import os
 import numpy as np
-
-def simpFDI(temp, humid, wind, df):
+from time import sleep
+#==============================================================================
+def simpFDI(temp, humd, wind, df):
     """
         Description
         -----------
-                Calculates the FDI via a simple formula.
+                Calculates the mk5 FDI via a simple formula.
                 
         Parameters
         ----------
@@ -29,18 +33,76 @@ def simpFDI(temp, humid, wind, df):
         -------
                 FDI : float
     """
-    return 2*(np.exp((0.987*np.log(df+0.001))-.45-(.0345*humid)+(.0338*temp)+(.0234*wind))) # simply calculated FDI
+    return 2*(np.exp((0.987*np.log(df+0.001))-.45-(.0345*humd)+(.0338*temp)+(.0234*wind))) # simply calculated FDI
 
-today = date.today()
-yesterday = today + timedelta(days=-1)
+def ratingFDI(FDI):
+    """
+        Description
+        -----------
+                Fetches a descriptor for the FDI based on NSW RFS standards.
+        
+        Parameters
+        ----------
+                FDI : float
+        
+        Returns
+        -------
+                descriptor : string
+    """    
+    
+    if (FDI >= 0) and (FDI < 12):
+        descriptor = 'LOW / MODERATE'
+    elif (FDI >= 12) and (FDI < 25):
+        descriptor = 'HIGH'
+    elif (FDI >= 25) and (FDI < 50):
+        descriptor = 'VERY HIGH'
+    elif (FDI >= 50) and (FDI < 75):
+        descriptor = 'SEVERE'
+    elif (FDI >= 75) and (FDI <100):
+        descriptor = 'EXTREME'
+    elif (FDI >= 100):
+        descriptor = 'CATASTROPHIC'
+    
+    return descriptor
 
-conn = sqlite3.connect(r"D:\code\fireWeather\weatherLog.db") # added 'r' - makes the file if it is not there?
+def ratingFWI(FWI):
+    """
+        Description
+        -----------
+                Fetches a descriptor for the FWI.
+        
+        Parameters
+        ----------
+                FDI : float
+        
+        Returns
+        -------
+                descriptor : string
+    """
+    if (FWI >= 0) and (FWI < 5):
+        descriptor = 'LOW'
+    elif (FWI >= 5) and (FWI < 14):
+        descriptor = 'MODERATE'
+    elif (FWI >= 14) and (FWI < 21):
+        descriptor = 'HIGH'
+    elif (FWI >= 21) and (FWI < 33):
+        descriptor = 'VERY HIGH'
+    elif (FWI >= 33):
+        descriptor = 'EXTREME'
+    
+    return descriptor
+
+#today = date.today()
+#yesterday = today + timedelta(days=-1)
+
+#conn = sqlite3.connect(r".\weatherLog.db") # added 'r' - makes the file if it is not there?
 #conn = sqlite3.connect(os.path.join('.', weatherLog.db)) # added 'r' - makes the file if it is not there?
-cur = conn.cursor()
 
-#cur.execute("CREATE TABLE observations ( date STRING, temp FLOAT, humid FLOAT, wind FLOAT, rain FLOAT )") # makes table of assumed requirments
-#cur.execute("CREATE TABLE calculations ( date STRING, ffmc FLOAT, dmc FLOAT, dc FLOAT, isi FLOAT, bui FLOAT, fwi FLOAT )") # makes table of assumed requirments
-#cur.execute("INSERT INTO calculations VALUES ('%s', 60, 25, 250, 0, 0, 0)"%(yesterday.strftime('%Y-%m-%d'))) # init values
+
+def initDatabase():
+    cur.execute("CREATE TABLE observations ( date STRING, temp FLOAT, humid FLOAT, wind FLOAT, rain FLOAT )") # makes table of assumed requirments
+    cur.execute("CREATE TABLE calculations ( date STRING, ffmc FLOAT, dmc FLOAT, dc FLOAT, isi FLOAT, bui FLOAT, fwi FLOAT )") # makes table of assumed requirments
+    cur.execute("INSERT INTO calculations VALUES ('%s', 60, 25, 250, 0, 0, 0)"%(yesterday.strftime('%Y-%m-%d'))) # initial values
 
 def is_dst(zonename):
     """
@@ -60,20 +122,39 @@ def is_dst(zonename):
     now = pytz.utc.localize(datetime.utcnow())
     return now.astimezone(tz).dst() != timedelta(0)
 
+def searchTimes():
+    """
+    """
+    print today.strftime('%Y-%m-%d')
+    search = today.strftime("%d") + '/'
+    search_yesterday = yesterday.strftime("%d") + '/'
+    if is_dst('Australia/Sydney'):
+        search_now = search + '11:00am'
+        search_yesterday = search_yesterday + '11:00am'
+    else:
+        search_now = search + '12:00pm'
+    #    search_now = search + '06:00am'
+        search_yesterday = search_yesterday + '12:00pm'
+    search_9am = date.today().strftime("%d") + '/09:00am'
+    return search_now, search_9am, search_yesterday
+
+#==============================================================================
+
+today = date.today()
+yesterday = today + timedelta(days=-1)
+
+if not os.path.exists("./weatherLog.db"):
+    initDatabase()
+    
+conn = sqlite3.connect(r".\weatherLog.db") # added 'r' - makes the file if it is not there?
+cur = conn.cursor()
+ 
+#==============================================================================
+
+print 'BOM/RICHMOND_AP' 
 tree = html.fromstring(urllib2.urlopen("http://www.bom.gov.au/products/IDN60801/IDN60801.95753.shtml").read())
-#today = date.today()
-#yesterday = today + timedelta(days=-1)
-print today.strftime('%Y-%m-%d')
-search = today.strftime("%d") + '/'
-search_yesterday = yesterday.strftime("%d") + '/'
-if is_dst('Australia/Sydney'):
-    search_now = search + '11:00am'
-    search_yesterday = search_yesterday + '11:00am'
-else:
-    search_now = search + '12:00pm'
-#    search_now = search + '06:00am'
-    search_yesterday = search_yesterday + '12:00pm'
-search_9am = date.today().strftime("%d") + '/09:00am'
+
+search_now, search_9am, search_yesterday = searchTimes()
 
 for e in tree.find_class('rowleftcolumn'):
     if e.getchildren()[0].text_content() == search_now:
@@ -112,7 +193,12 @@ print "DMC  : %.2f"%dmc
 print "DC   : %.2f"%dc
 print "ISI  : %.2f"%isi
 print "BUI  : %.2f"%bui
-print "FWI  : %.2f"%fwi
+print "FWI  : %.2f %s"%(fwi, ratingFWI(fwi))
 df = 5 # no remote scraping yet
-print "FDI  : %.2f"%simpFDI(temp, humd, wind, df)
+FDI = simpFDI(temp, humd, wind, df)
+print "FDI  : %.2f %s"%(FDI, ratingFDI(FDI))
 print "\r\n-----------------------------\r\n"
+
+#sleep(10)
+
+#raw_input('Holding')
